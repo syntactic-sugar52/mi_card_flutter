e1dd90381a82b7c01bbc063aede3a6f9b8b4361c
kd ang <kdang82590@gmail.com>
2020-10-25 23:07:14 -0500

    digital business card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,91 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage("image/icons8-edit-profile-64.png"),
+              ),
+              Text(
+                'KD ANG',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 1.5,
+                    color: Colors.teal.shade100),
+              ),
+              SizedBox(
+                height:20.0,
+                width: 150.0,
+                child: Divider(
+                  color:Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          '+639164860168',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.teal.shade900,
+                              fontFamily: 'SansSourcePro'),
+                        )),
+                  )),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'kdang62@yahii.com',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.teal.shade900,
+                              fontFamily: 'SansSourcePro'),
+                        )),
+                  )),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
